222c8e3d0ecbfaa72f57b663eb1923cb08c7e3a2
Joaquim Gregório <joaquim.ibi@gmail.com>
2022-07-02 10:23:00 -0300

    update: type annotation

---
+++ src/001-type-annotation/index.ts
@@ -0,0 +1,23 @@
+/* eslint-disable */
+// Basic types (inferred types)
+const name: string = 'Joaquim';
+const age: number = 19;
+const adult: boolean = true;
+const hisSimbol: symbol = Symbol('any-symbol');
+
+// Arrays
+const numberArray: Array<number> = [1, 2, 3];
+const stringArray: string[] = ['hello', 'world'];
+
+// Objects
+const person: { name: string; age: number; adult?: boolean } = {
+  name: 'Joaquim',
+  age: 19,
+};
+
+// Function
+function sum(x: number, y: number): number {
+  return x + y;
+}
+
+const quocient: (x: number, y: number) => number = (x, y) => x / y;
